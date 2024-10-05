b0206b4cb3ae010a751e97ab22e890f792971911
Adam <adamf26051998@gmail.com>
2024-10-05 13:31:11 +0100

    Implemented a resilient http client to retry and exponentially backoff on 429 HTTP status codes

--- src/Hooki.IntegrationTests/Config/HttpClientFixture.cs
+++ src/Hooki.IntegrationTests/Config/HttpClientFixture.cs
@@ -1,4 +1,8 @@
+using System.Net;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace IntegrationTests.Config;
 
@@ -9,8 +13,8 @@ public class HttpClientFixture : IDisposable
 
     public HttpClientFixture()
     {
-        Client = new HttpClient();
-
+        var services = new ServiceCollection();
+        
         var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development";
 
         if (environment is "Development")
@@ -23,6 +27,30 @@ public class HttpClientFixture : IDisposable
             .AddJsonFile("appsettings.json")
             .AddEnvironmentVariables()
             .Build();
+        
+        services.AddHttpClient("ResilientClient")
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetCircuitBreakerPolicy());
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+        Client = httpClientFactory.CreateClient("ResilientClient");
+    }
+    
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == HttpStatusCode.TooManyRequests)
+            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
     }
 
     public void Dispose()
